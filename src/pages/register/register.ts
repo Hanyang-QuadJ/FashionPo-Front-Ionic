e7d658ed22e6f649faab5028ff773377d5674565
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-07-26 17:45:54 +0900

    마이페이지

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import {Http, Headers} from "@angular/http";
+import 'rxjs/add/operator/map';
+import { Storage } from '@ionic/storage';
+import {TabsPage} from "../tabs/tabs";
 /**
  * Generated class for the RegisterPage page.
  *
@@ -12,16 +15,27 @@ import { NavController, NavParams } from 'ionic-angular';
   selector: 'page-register',
   templateUrl: 'register.html',
 })
+
 export class RegisterPage {
+  user: object = {};
+  option: string = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage, private http: Http) {
+    this.option = "view";
+    this.storage.get('token').then((val) => {
+      let headers = new Headers();
+      headers.append('Content-Type', 'text/plain');
+      headers.append('x-access-token', val);
+      // console.log(val);
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
+      this.http.get('/user/authed', {headers: headers})
+        .map(res => res.json())
+        .subscribe(data => {
+          this.user = data.user[0];
+        });
+    });
   }
-  ionViewWillEnter() {
-    console.log('reload PlaygroundPage');
-  }
-
 }
+
+
+
