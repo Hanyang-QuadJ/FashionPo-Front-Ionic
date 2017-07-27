baff82bc0cedcf7ddec7725e9d42ebbd7845f9d0
JeongHyunLee <nomar0107@gmail.com>
2017-07-27 12:56:18 +0900

    login err handling

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, Platform } from 'ionic-angular';
-import { Http, Headers } from '@angular/http';
+import {Component} from '@angular/core';
+import {NavController, NavParams, Platform} from 'ionic-angular';
+import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/map';
-import { Storage } from '@ionic/storage';
+import {Storage} from '@ionic/storage';
 import {TabsPage} from "../tabs/tabs";
+import { ToastController } from 'ionic-angular';
+
 /**
  * Generated class for the LoginPage page.
  *
@@ -13,37 +15,58 @@ import {TabsPage} from "../tabs/tabs";
  */
 
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+    selector: 'page-login',
+    templateUrl: 'login.html',
 })
 
 export class LoginPage {
-  login = {
-    email: '',
-    password: ''
-  };
-  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http, private storage: Storage, public platform: Platform) {
-
-  }
-
-  userLogin() {
-    var APIUrl = '/auth';
-    // if (this.platform.is('ios') == true){
-    //   APIUrl = 'http://54.162.160.91/api/auth';
-    //   // console.log('yes');
-    // }
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    let body = {
-      email : this.login.email,
-      password : this.login.password
+    login = {
+        email: '',
+        password: ''
     };
-    this.http.post(APIUrl + "/login", JSON.stringify(body), {headers: headers})
-      .map(res => res.json())
-      .subscribe(data => {
-        this.storage.set('token', data.token);
-        this.navCtrl.push(TabsPage);
-      });
-  }
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http, private storage: Storage, public platform: Platform,public toastCtrl: ToastController) {
+
+    }
+
+
+
+    showToast(position: string) {
+        let toast = this.toastCtrl.create({
+            message: 'invalid user info',
+            duration: 2000,
+            position: position
+        });
+
+        toast.present(toast);
+    }
+
+
+    userLogin() {
+        var APIUrl = '/auth';
+        // if (this.platform.is('ios') == true){
+        //   APIUrl = 'http://54.162.160.91/api/auth';
+        //   // console.log('yes');
+        // }
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        let body = {
+            email: this.login.email,
+            password: this.login.password
+        };
+        this.http.post(APIUrl + "/login", JSON.stringify(body), {headers: headers})
+            .map(res => res.json())
+            .subscribe(
+                data => {
+                    this.storage.set('token', data.token);
+                    this.navCtrl.push(TabsPage);
+                },
+                err => {
+                    this.showToast("middle");
+
+                });
+    }
+
+
 
 }
