1b936dcd84f0d492197cd80f783a0e564a705c4d
ShinHyunJong <kashiashin@gmail.com>
2017-07-27 13:55:44 +0900

    Rank Page Design

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -36,6 +36,9 @@ export class HomePage {
       this.http.get(APIUrl, {headers: headers})
         .map(res => res.json())
         .subscribe(data => {
+          for(var i=1;i<data.posts.length;i++) {
+            this.ranks[i-1] = data.posts[i];
+          }
           this.picURL = data.posts[0].picURL;
         });
     });
@@ -55,6 +58,9 @@ export class HomePage {
       this.http.get(APIUrl, {headers: headers})
         .map(res => res.json())
         .subscribe(data => {
+          for(var i=1;i<data.posts.length;i++) {
+            this.ranks[i-1] = data.posts[i];
+          }
           this.picURL = data.posts[0].picURL;
         });
     });
