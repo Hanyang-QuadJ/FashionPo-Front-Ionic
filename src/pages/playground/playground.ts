4bc3e9dbfb32fd4e9345bc3bbcfc5278ac0d331c
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-07-31 00:55:05 +0900

    사진 + 갤러리

--- src/pages/playground/playground.ts
+++ src/pages/playground/playground.ts
@@ -23,8 +23,8 @@ declare var cordova: any;
 })
 
 export class PlaygroundPage implements OnInit{
-  lastImage: string = null;
-  loading: Loading;
+  public base64Image: any;
+
   constructor(private http: Http,
               private storage : Storage,
               private camera: Camera,
@@ -62,103 +62,33 @@ export class PlaygroundPage implements OnInit{
     });
     actionSheet.present();
   }
+  pictureTaken :boolean = false;
 
-  public takePicture(sourceType) {
-    // Create options for the Camera Dialog
-    var options = {
-      quality: 100,
-      sourceType: sourceType,
-      saveToPhotoAlbum: false,
-      correctOrientation: true
-    };
+  public takePicture(sourceType){
+      let options = {
+        targetWidth: 500,
+        targetHeight: 500,
+        quality: 100,
+        allowEdit: true,
+        correctOrientation: false,
+        saveToPhotoAlbum: false,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+        sourceType: sourceType,
+      };
+      this.camera.getPicture(options).then((imageData) => {
+        this.pictureTaken = true;
+        // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+        let cameraImageSelector = document.getElementById('camera-image');
+        cameraImageSelector.setAttribute('src', this.base64Image);
+      }, (err) => {
+        console.log(err);
+      });
 
-    // Get the data of an image
-    this.camera.getPicture(options).then((imagePath) => {
-      // Special handling for Android library
-      if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
-        this.filePath.resolveNativePath(imagePath)
-          .then(filePath => {
-            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
-            let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
-            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
-          });
-      } else {
-        var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
-        var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
-        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
-      }
-    }, (err) => {
-      this.presentToast('Error while selecting image.');
-    });
-  }
-  // Create a new name for the image
-  private createFileName() {
-    var d = new Date(),
-      n = d.getTime(),
-      newFileName =  n + ".jpg";
-    return newFileName;
   }
 
-// Copy the image to a local folder
-  private copyFileToLocalDir(namePath, currentName, newFileName) {
-    this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
-      this.lastImage = newFileName;
-    }, error => {
-      this.presentToast('Error while storing file.');
-    });
-  }
-
-  private presentToast(text) {
-    let toast = this.toastCtrl.create({
-      message: text,
-      duration: 3000,
-      position: 'top'
-    });
-    toast.present();
-  }
-
-// Always get the accurate path to your apps folder
-  public pathForImage(img) {
-    if (img === null) {
-      return '';
-    } else {
-      return cordova.file.dataDirectory + img;
-    }
-  }
-  // public uploadImage() {
-  //   // Destination URL
-  //   var url = "http://yoururl/upload.php";
-  //
-  //   // File for Upload
-  //   var targetPath = this.pathForImage(this.lastImage);
-  //
-  //   // File name only
-  //   var filename = this.lastImage;
-  //
-  //   var options = {
-  //     fileKey: "file",
-  //     fileName: filename,
-  //     chunkedMode: false,
-  //     mimeType: "multipart/form-data",
-  //     params : {'fileName': filename}
-  //   };
-  //
-  //   const fileTransfer: TransferObject = this.transfer.create();
-  //
-  //   this.loading = this.loadingCtrl.create({
-  //     content: 'Uploading...',
-  //   });
-  //   this.loading.present();
-  //
-  //   // Use the FileTransfer to upload the image
-  //   fileTransfer.upload(targetPath, url, options).then(data => {
-  //     this.loading.dismissAll()
-  //     this.presentToast('Image succesful uploaded.');
-  //   }, err => {
-  //     this.loading.dismissAll()
-  //     this.presentToast('Error while uploading file.');
-  //   });
-  // }
   ngOnInit(): void {
 
   }
