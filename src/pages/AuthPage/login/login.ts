0d82695bcdcd92ffec0bee0fffd783d917505363
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-08-08 21:10:39 +0900

    프로필업로드

--- src/pages/AuthPage/login/login.ts
+++ src/pages/AuthPage/login/login.ts
@@ -18,76 +18,77 @@ import {SignupPage} from "../signup/signup";
  */
 
 @Component({
-    selector: 'page-login',
-    templateUrl: 'login.html',
+  selector: 'page-login',
+  templateUrl: 'login.html',
 })
 
 export class LoginPage {
 
-    loginForm : FormGroup;
+  loginForm : FormGroup;
 
-    constructor(public navCtrl: NavController,
-                public navParams: NavParams,
-                private http: Http,
-                private storage: Storage,
-                public platform: Platform,
-                public toastCtrl: ToastController,
-                public fb: FormBuilder) {
-      this.loginForm = this.fb.group({
-        email: ['', Validators.compose([Validators.pattern("[a-zA-Z0-9]+@fitnyc.edu"),Validators.required])],
-        password: ['', Validators.compose([Validators.minLength(12), Validators.required])]
-      });
-    }
-
-  ngOnInit(): void {
-    this.storage.get('token').then((val) => {
-      const token = val;
-      if (token != null && token != '') {
-
-        this.navCtrl.setRoot(TabsPage);
-      }
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private http: Http,
+              private storage: Storage,
+              public platform: Platform,
+              public toastCtrl: ToastController,
+              public fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.compose([Validators.pattern("[a-zA-Z0-9]+@fitnyc.edu"),Validators.required])],
+      password: ['', Validators.compose([Validators.minLength(12), Validators.required])]
     });
   }
-  goToSignup() {
-      this.navCtrl.setRoot(SignupPage);
+
+  ngOnInit(): void {
+    // this.storage.get('token').then((val) => {
+    //   const token = val;
+    //   if (token != null && token != '') {
+    //
+    //     this.navCtrl.setRoot(TabsPage);
+    //   }
+    // });
   }
-    showToast(position: string) {
-        let toast = this.toastCtrl.create({
-            message: 'Check your email or password',
-            duration: 2000,
-            position: position
+  goToSignup() {
+    this.navCtrl.setRoot(SignupPage);
+  }
+  showToast(position: string) {
+    let toast = this.toastCtrl.create({
+      message: 'Check your email or password',
+      duration: 2000,
+      position: position
+    });
+
+    toast.present(toast);
+  }
+
+
+  userLogin() {
+    var APIUrl = '/auth';
+
+
+    if (this.platform.is('ios') == true){
+      APIUrl = 'http://54.162.160.91/api/auth';
+      // console.log('yes');
+    }
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let body = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    };
+    this.http.post(APIUrl + "/login", JSON.stringify(body), {headers: headers})
+      .map(res => res.json())
+      .subscribe(
+        data => {
+          this.storage.set('token', data.token);
+          this.navCtrl.setRoot(TabsPage);
+        },
+        err => {
+          this.showToast("bottom");
         });
-
-        toast.present(toast);
-    }
-
-
-    userLogin() {
-        var APIUrl = '/auth';
-
-
-        // if (this.platform.is('ios') == true){
-        //   APIUrl = 'http://54.162.160.91/api/auth';
-        //   // console.log('yes');
-        // }
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        let body = {
-            email: this.loginForm.value.email,
-            password: this.loginForm.value.password
-        };
-        this.http.post(APIUrl + "/login", JSON.stringify(body), {headers: headers})
-            .map(res => res.json())
-            .subscribe(
-                data => {
-                    this.storage.set('token', data.token);
-                    this.navCtrl.setRoot(TabsPage);
-                },
-                err => {
-                    this.showToast("bottom");
-                });
-    }
+  }
 
 
 
 }
+
--- src/pages/AuthPage/signup/signup.ts
+++ src/pages/AuthPage/signup/signup.ts
@@ -1,16 +1,15 @@
 import {Component} from '@angular/core';
-import {ActionSheetController, LoadingController, NavController, NavParams, Platform} from 'ionic-angular';
+import {ActionSheetController, NavController, NavParams, Platform} from 'ionic-angular';
 import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/map';
 import {Storage} from '@ionic/storage';
-import {TabsPage} from "../../tabs/tabs";
 import { ToastController } from 'ionic-angular';
-import {FormBuilder, FormGroup, Validator, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Camera} from "@ionic-native/camera";
+import {LoginPage} from "../login/login";
 import {FilePath} from "@ionic-native/file-path";
 import {Transfer} from "@ionic-native/transfer";
-import {Camera} from "@ionic-native/camera";
-
 
 /**
  * Generated class for the LoginPage page.
@@ -32,13 +31,9 @@ export class SignupPage {
   constructor(private http: Http,
               private storage : Storage,
               private camera: Camera,
-              private transfer: Transfer,
-              private file: File,
-              private filePath: FilePath,
               public actionSheetCtrl: ActionSheetController,
               public toastCtrl: ToastController,
               public platform: Platform,
-              public loadingCtrl: LoadingController,
               public navCtrl: NavController,
               public navParams: NavParams,
               public fb: FormBuilder) {
@@ -52,30 +47,30 @@ export class SignupPage {
   ngOnInit(): void {
 
   }
-  // public presentActionSheet() {
-  //   let actionSheet = this.actionSheetCtrl.create({
-  //     title: 'Select Image Source',
-  //     buttons: [
-  //       {
-  //         text: 'Load from Library',
-  //         handler: () => {
-  //           this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
-  //         }
-  //       },
-  //       {
-  //         text: 'Use Camera',
-  //         handler: () => {
-  //           this.takePicture(this.camera.PictureSourceType.CAMERA);
-  //         }
-  //       },
-  //       {
-  //         text: 'Cancel',
-  //         role: 'cancel'
-  //       }
-  //     ]
-  //   });
-  //   actionSheet.present();
-  // }
+  public presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select Image Source',
+      buttons: [
+        {
+          text: 'Load from Library',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        },
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.CAMERA);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
   showToast(position: string, message: string) {
     let toast = this.toastCtrl.create({
       message: message,
@@ -84,57 +79,57 @@ export class SignupPage {
     });
   }
 
-    userSignup() {
-      var APIUrl = '/auth';
+  userSignup() {
+    var APIUrl = '/auth';
 
-
-      if (this.platform.is('ios') == true) {
-        APIUrl = 'http://54.162.160.91/api/auth';
-        // console.log('yes');
-      }
-      let headers = new Headers();
-      headers.append('Content-Type', 'application/json');
-      let body = {
-        email: this.loginForm.value.email,
-        username: this.loginForm.value.username,
-        password: this.loginForm.value.password,
-        base_64: this.base64Image
-      };
-      this.http.post(APIUrl + "/register", JSON.stringify(body), {headers: headers})
-        .map(res => res.json())
-        .subscribe(
-          data => {
-            this.showToast("bottom", "User successfully created");
-            this.navCtrl.pop();
-          },
-          err => {
-            this.showToast("bottom", "error occured");
-          });
+    if (this.platform.is('ios') == true) {
+      APIUrl = 'http://54.162.160.91/api/auth';
+      // console.log('yes');
     }
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let body = {
+      email: this.loginForm.value.email,
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password,
+      base_64: this.base64Image
+    };
+    this.http.post(APIUrl + "/register", JSON.stringify(body), {headers: headers})
+      .map(res => res.json())
+      .subscribe(
+        data => {
+          this.showToast("bottom", "User successfully created");
+          this.navCtrl.setRoot(LoginPage);
+        },
+        err => {
+          this.showToast("bottom", "error occured");
+        });
+  }
 
   pictureTaken :boolean = false;
 
-  // public takePicture(sourceType){
-  //   let options = {
-  //     targetWidth: 500,
-  //     targetHeight: 500,
-  //     quality: 70,
-  //     allowEdit: true,
-  //     correctOrientation: false,
-  //     saveToPhotoAlbum: false,
-  //     destinationType: this.camera.DestinationType.DATA_URL,
-  //     encodingType: this.camera.EncodingType.JPEG,
-  //     mediaType: this.camera.MediaType.PICTURE,
-  //     sourceType: sourceType,
-  //   };
-  //   this.camera.getPicture(options).then((imageData) => {
-  //     this.pictureTaken = true;
-  //     // imageData is a base64 encoded string
-  //     this.base64Image = "data:image/jpeg;base64," + imageData;
-  //     let cameraImageSelector = document.getElementById('camera-image');
-  //     cameraImageSelector.setAttribute('src', this.base64Image);
-  //   })
-  //
-  // }
+  public takePicture(sourceType){
+    let options = {
+      targetWidth: 500,
+      targetHeight: 500,
+      quality: 70,
+      allowEdit: true,
+      correctOrientation: false,
+      saveToPhotoAlbum: false,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: sourceType,
+    };
+    this.camera.getPicture(options).then((imageData) => {
+      this.pictureTaken = true;
+      // imageData is a base64 encoded string
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+      let cameraImageSelector = document.getElementById('camera-image');
+      cameraImageSelector.setAttribute('src', this.base64Image);
+    })
+
+  }
 
 }
+
