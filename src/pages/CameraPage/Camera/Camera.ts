24309d7f5497608608d3951fe0af76c53aacb36a
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-08-09 09:18:00 +0900

    태그수젇ㅇ

--- src/pages/CameraPage/Camera/Camera.ts
+++ src/pages/CameraPage/Camera/Camera.ts
@@ -26,7 +26,7 @@ declare var cordova: any;
 export class CameraPage implements OnInit{
   public base64Image: any;
 
-  tags: Array<any> = [];
+  tags: any = [];
 
   public tagsInput:any;
 
@@ -163,42 +163,39 @@ export class CameraPage implements OnInit{
     console.log(this.tags)
 
 
-    // var APIUrl = '/post';
+    var APIUrl = '/post';
     // if (this.platform.is('ios') == true){
     //   APIUrl = 'http://54.162.160.91/api/post';
     //   // console.log('yes');
     // }
-    // this.storage.get('token').then((val) => {
-    //   let headers = new Headers();
-    //   headers.append('Content-Type', 'application/json');
-    //   headers.append('x-access-token', val);
-    //   // console.log(val);
-    //
-    //   for(var i = 0; i<this.tags.length;i++){
-    //     this.tagsArray=this.tags[i]
-    //   }
-    //   let body = {
-    //     // base_64: this.base64Image,
-    //     tags:this.tags
-    //
-    //   };
-    //   this.http.post(APIUrl + "/create", JSON.stringify(body), {headers: headers})
-    //     .map(res => res.json())
-    //     .subscribe(
-    //       data => {
-    //         let toast = this.toastCtrl.create({
-    //           message: 'upload success',
-    //           duration: 2000
-    //         });
-    //
-    //         toast.present(toast);
-    //       },
-    //       err => {
-    //
-    //       });
-    // }, (err) => {
-    //   console.log(err);
-    // });
+    this.storage.get('token').then((val) => {
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      headers.append('x-access-token', val);
+      // console.log(val);
+
+      let body = {
+        // base_64: this.base64Image,
+        tags:this.tags
+
+      };
+      this.http.post(APIUrl + "/create", JSON.stringify(body), {headers: headers})
+        .map(res => res.json())
+        .subscribe(
+          data => {
+            let toast = this.toastCtrl.create({
+              message: 'upload success',
+              duration: 2000
+            });
+
+            toast.present(toast);
+          },
+          err => {
+
+          });
+    }, (err) => {
+      console.log(err);
+    });
 
 
 
