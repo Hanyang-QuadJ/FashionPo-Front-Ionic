40f949f1352010f3745347cfbc25451656282edd
JeongHyunLee <nomar0107@gmail.com>
2017-08-12 20:10:08 +0900

    asdf

--- src/pages/WardrobePage/Wardrobe/Wardrobe.ts
+++ src/pages/WardrobePage/Wardrobe/Wardrobe.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {NavController, NavParams, Platform, ModalController,LoadingController} from 'ionic-angular';
+import {NavController, NavParams, Platform, ModalController, LoadingController} from 'ionic-angular';
 import {Http, Headers} from "@angular/http";
 import 'rxjs/add/operator/map';
 import {Storage} from '@ionic/storage';
@@ -27,35 +27,37 @@ import {WardrobeThisWeekPage} from '../../WardrobePage/wardrobe-this-week/wardro
 
 
 export class WardrobePage {
-  public toggled: boolean;
+    public toggled: boolean;
 
     user: object = {};
-    loaded:boolean;
-    loadedd:boolean;
+    loaded: boolean;
+    loadedd: boolean;
     mypostlist: Array<object> = [];
-    thisWeekPost: Array<object> =[];
+    thisWeekPost: Array<object> = [];
     option: string = "";
     myposts: string = "";
     favorites: Array<object> = [];
     favoritesLength: string = "";
-    postAlert:string="";
-    tab1:any = PostTabPage;
-    tab2:any = FavoriteTabPage;
-    date:Array<string>=[];
-    date2:Array<string>=[];
-    dateFinal:Array<object>=[];
-    checkThis=0;
-    alertThis:boolean;
+    postAlert: string = "";
+    tab1: any = PostTabPage;
+    tab2: any = FavoriteTabPage;
+    date: Array<string> = [];
+    date2: Array<string> = [];
+    dateFinal: Array<object> = [];
+    checkThis = 0;
+    alertThis: boolean;
+    view_cnt: any;
+    button_loaded: boolean = false;
+    today_disable: boolean = false;
 
 
-    year:Array<any>=[];
-    year2:Array<any>=[];
-    startDay:Array<any>=[];
-    endDay:Array<any>=[];
-    endDay2:Array<any>=[];
-    month:Array<any>=[];
-    month2:Array<any>=[];
-
+    year: Array<any> = [];
+    year2: Array<any> = [];
+    startDay: Array<any> = [];
+    endDay: Array<any> = [];
+    endDay2: Array<any> = [];
+    month: Array<any> = [];
+    month2: Array<any> = [];
 
 
     constructor(public navCtrl: NavController,
@@ -67,116 +69,148 @@ export class WardrobePage {
                 public platform: Platform) {
 
 
+    }
 
-
-
+    toggleRank() {
+        this.toggled = this.toggled ? false : true;
 
     }
 
-  toggleRank() {
-    this.toggled = this.toggled ? false : true;
-
-  }
     Settings() {
 
-      this.navCtrl.push(SettingsPage,{users:this.user},{});
+        this.navCtrl.push(SettingsPage, {users: this.user}, {});
     }
- ionViewWillEnter(){
-   console.log("11111111111")
 
-   this.mypostlist=[]
-   this.thisWeekPost=[]
+    ionViewWillEnter() {
+        console.log("11111111111")
 
-
-
-   this.loaded = false;
-   this.loadedd = false;
-
-   this.option = "favorites";
-   let loading = this.loadingCtrl.create({showBackdrop:false,spinner:'crescent',
-
-   });
-
-   loading.present();
-   this.storage.get('token').then((val) => {
-     var APIUrl = '/user';
-     var APIUrl_2 = '/post';
-     // if (this.platform.is('ios') == true){
-     //   APIUrl = 'http://54.162.160.91/api/user';
-     //   APIUrl_2 = 'http://54.162.160.91/api/post';
-     // }
-
-     let headers = new Headers();
-     headers.append('Content-Type', 'application/json');
-     headers.append('x-access-token', val);
-     // console.log(val);
-
-     this.http.get(APIUrl+'/authed', {headers: headers})
-       .map(res => res.json())
-       .subscribe(data => {
-         this.user = data.user[0];
-
-       });
+        this.mypostlist = []
+        this.thisWeekPost = []
 
 
-     this.http.get(APIUrl_2+'/myposts', {headers: headers})
-       .map(res => res.json())
-       .subscribe(data => {
+        this.loaded = false;
+        this.loadedd = false;
+
+        this.option = "favorites";
+        let loading = this.loadingCtrl.create({
+            showBackdrop: false, spinner: 'crescent',
+
+        });
+
+        loading.present();
+        this.storage.get('token').then((val) => {
+            var APIUrl = '/user';
+            var APIUrl_2 = '/post';
+            // if (this.platform.is('ios') == true){
+            //   APIUrl = 'http://54.162.160.91/api/user';
+            //   APIUrl_2 = 'http://54.162.160.91/api/post';
+            // }
+
+            let headers = new Headers();
+            headers.append('Content-Type', 'application/json');
+            headers.append('x-access-token', val);
+            // console.log(val);
+
+            this.http.get(APIUrl + '/authed', {headers: headers})
+                .map(res => res.json())
+                .subscribe(data => {
+                    this.user = data.user[0];
+                    this.button_loaded = true;
+                    console.log("@#################");
+                    console.log(this.button_loaded);
+                    console.log(this.today_disable);
+                    console.log("@#################");
+                });
 
 
-           for (var i = 0; i<data.posts.length;i++){
-             //이번주 사진
-             if(data.posts[i].isThisWeek===true){
-               this.thisWeekPost.push(data.posts[i])
-             }
-             //모든 사진
-             else {
-               this.mypostlist.push(data.posts[i]);
-             }
-
-         }
-
-         this.loaded = true;
+            this.http.get(APIUrl_2 + '/myposts', {headers: headers})
+                .map(res => res.json())
+                .subscribe(data => {
 
 
+                    for (var i = 0; i < data.posts.length; i++) {
+                        //이번주 사진
+                        if (data.posts[i].isThisWeek === true) {
+                            this.thisWeekPost.push(data.posts[i])
+                        }
+                        //모든 사진
+                        else {
+                            this.mypostlist.push(data.posts[i]);
+                        }
 
-       });
+                    }
 
-     this.http.get(APIUrl+'/favorite', {headers: headers})
-       .map(res => res.json())
-       .subscribe(data => {
-         if(data.favorites === undefined || data.favorites.length == 0){
-           this.favorites = [];
-           this.loadedd = true;
-           loading.dismiss();
-         }
-         else{
-           const body = {users:data.favorites};
-           this.http.post(APIUrl, JSON.stringify(body), {headers: headers})
-             .map(res => res.json())
-             .subscribe(
-               data => {
-                 this.favorites = data;
-                 this.loadedd = true;
-                 loading.dismiss();
-               });
+                    this.loaded = true;
 
-         }
-       });
 
-   });
+                });
 
- }
-    ionViewDidLoad(){
+            this.http.get(APIUrl + '/favorite', {headers: headers})
+                .map(res => res.json())
+                .subscribe(data => {
+                    if (data.favorites === undefined || data.favorites.length == 0) {
+                        this.favorites = [];
+                        this.loadedd = true;
+                        loading.dismiss();
+                    }
+                    else {
+                        const body = {users: data.favorites};
+                        this.http.post(APIUrl, JSON.stringify(body), {headers: headers})
+                            .map(res => res.json())
+                            .subscribe(
+                                data => {
+                                    this.favorites = data;
+                                    this.loadedd = true;
+                                    loading.dismiss();
+                                });
+
+                    }
+                });
+
+        });
 
     }
-    ionViewDidLeave(){
-      this.mypostlist=[]
-      this.thisWeekPost=[]
+
+    ionViewDidLoad() {
+
     }
-    presentThisWeekModal(i){
-      let thisWeekModal = this.modalCtrl.create(WardrobeThisWeekPage,{thisWeekPost:this.thisWeekPost,thisWeekPostIndex:'fit'+i},{leaveAnimation:'back'});
-      thisWeekModal.present();
+
+    ionViewDidLeave() {
+        this.mypostlist = []
+        this.thisWeekPost = []
+    }
+
+    presentThisWeekModal(i) {
+        let thisWeekModal = this.modalCtrl.create(WardrobeThisWeekPage, {
+            thisWeekPost: this.thisWeekPost,
+            thisWeekPostIndex: 'fit' + i
+        }, {leaveAnimation: 'back'});
+        thisWeekModal.present();
+    }
+
+    refreshViewCnt() {
+        this.today_disable = true;
+        this.button_loaded = false;
+        this.storage.get('token').then((val) => {
+            var APIUrl = '/user';
+            // if (this.platform.is('ios') == true){
+            //   APIUrl = 'http://54.162.160.91/api/user';
+            //   // console.log('yes');
+            // }
+            let headers = new Headers();
+            headers.append('Content-Type', 'application/json');
+            headers.append('x-access-token', val);
+            this.http.get(APIUrl + '/authed', {headers: headers})
+                .map(res => res.json())
+                .subscribe(data => {
+                    this.user = data.user[0];
+
+                    this.button_loaded = true;
+                    this.today_disable = false;
+
+
+                })
+        });
     }
 
 }
